6ad0fb89953b7285d50dc1576e48426a9582f858
AbstractShard <165806563+AbstractShard@users.noreply.github.com>
2025-02-17 17:25:33 +0300

    Improvements: - Cleaned Player code

--- Code/player.py
+++ Code/player.py
@@ -1,57 +1,54 @@
 import pygame
 import basic
 
-KEYBOARD = {"UP": pygame.K_w, "RIGHT": pygame.K_d, "DOWN": pygame.K_s, "LEFT": pygame.K_a}
-MOUSE = {"LEFT": 0, "MIDDLE": 1, "RIGHT": 2}
+# BINDS
+MOVEMENT = {pygame.K_w: (0, -1), pygame.K_d: (1, 0), pygame.K_s: (0, 1), pygame.K_a: (-1, 0)}
+ATTACKS = {("MOUSE", 0): "TESTATTACK"}
 
-ATTACK_BINDS = {("m", 0): "TestAttack"}
-SIMULT_ATTACKS = 1
+# MOVEMENT
+SPEED = 1
 
 # region FUNCTIONS
-def check_move() -> str:
-    for key in KEYBOARD.values():
-        if pygame.key.get_pressed()[key]:
-            return "Move"
-    return ""
+def to_movement() -> bool:
+    for bind in MOVEMENT.keys():
+        if pygame.key.get_pressed()[bind]:
+            return True
+    return False
 
-def check_attacks() -> str:
-    for bind_type, value in ATTACK_BINDS.keys():
-        if (bind_type == "k" and pygame.key.get_pressed()[value]) or (bind_type == "m" and pygame.mouse.get_pressed()[value]):
-            state = STATES[ATTACK_BINDS[(bind_type, value)]]
+def to_attack() -> str:
+    for bind_type, bind in ATTACKS.keys():
+        if (bind_type == "KEYBOARD" and pygame.key.get_pressed()[bind]) or (bind_type == "MOUSE" and pygame.mouse.get_pressed()[bind]):
+            atk = STATES[ATTACKS[(bind_type, bind)]]
 
-            if not state.ATTACK.counter["cooldown"] or state.ATTACK.counter["cooldown"] == state.ATTACK.cooldown:
-                return state.__class__.__name__
+            if not atk.counter["cooldown"] or atk.counter["cooldown"] == atk.cooldown:
+                return atk.__class__.__name__.upper()
     return ""
 # endregion
 
 # region STATES
 class Idle(basic.State):
     def run(self, parent) -> str:
-        if move := check_move():
-            return move
+        if to_movement():
+            return "MOVE"
 
-        if atk := check_attacks():
+        if atk := to_attack():
             return atk
 
         return ""
 
 
 class Move(basic.State):
-    def __init__(self):
-        self.MOVE_DIRS = {"UP": (0, -1), "RIGHT": (1, 0), "DOWN": (0, 1), "LEFT": (-1, 0)}
-        self.SPEED = 1
-
     def run(self, parent) -> str:
-        move_keys = [key for key, value in KEYBOARD.items() if pygame.key.get_pressed()[value]]
+        move_dirs = [dir for bind, dir in MOVEMENT.items() if pygame.key.get_pressed()[bind]]
 
-        if not move_keys:
-            return "Idle"
+        if not move_dirs:
+            return "IDLE"
 
-        if atk := check_attacks():
+        if atk := to_attack():
             return atk
 
-        for key in move_keys:
-            move_vec = pygame.math.Vector2(self.MOVE_DIRS[key]).normalize() * self.SPEED
+        for dir in move_dirs:
+            move_vec = pygame.math.Vector2(dir).normalize() * SPEED
 
             parent.rect.move_ip(move_vec)
             parent.update_collider()
@@ -65,25 +62,26 @@ class Move(basic.State):
         return ""
 
 
-class TestAttack(basic.State):
+class TestAttack(basic.Attack, basic.State):
     def __init__(self):
-        self.ATTACK = basic.Attack((25, 5), (25, 5), "rect", 15, 25, 50, 50)
+        basic.Attack.__init__(self, (25, 5), (25, 5), "rect", 15, 25, 50, 50)
+        basic.State.__init__(self)
 
     def run(self, parent) -> str:
-        if self.ATTACK.counter["frames"] <= 0 < self.ATTACK.counter["cooldown"]:
-            return "Idle"
+        if self.counter["frames"] <= 0 < self.counter["cooldown"]:
+            return "IDLE"
 
-        if len(parent.curr_attacks) < SIMULT_ATTACKS and self.ATTACK not in parent.curr_attacks:
-            parent.make_attack(self.ATTACK)
+        if self not in parent.curr_attacks:
+            parent.make_attack(self)
 # endregion
 
 
-STATES = {"Idle": Idle(), "Move": Move(), "TestAttack": TestAttack()}
+STATES = {"IDLE": Idle(), "MOVE": Move(), "TESTATTACK": TestAttack()}
 
 
 class Player(basic.StateMachine, basic.Entity):
     def __init__(self, groups: list, collide_with: list, to_attack: list, pos=(0, 0)):
-        basic.StateMachine.__init__(self, STATES, "Idle")
+        basic.StateMachine.__init__(self, STATES, "IDLE")
         basic.Entity.__init__(self, groups, collide_with, to_attack, pos, 100, (10, 20), True)
 
         self.setup_basics(pos, tsize=(10, 20), tcolor="red")
